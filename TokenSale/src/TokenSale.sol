8bc1d9a5b36b546bd0ebc38bcda6c30fd5b1632a
Mario Karagiorgas <salem8@gmail.com>
2025-05-27 22:13:24 +0300

    feat: TokenSale

--- TokenSale/src/TokenSale.sol
+++ TokenSale/src/TokenSale.sol
@@ -10,13 +10,19 @@ import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 // users should also be able to buy tokens by sending ether to the contract
 // then the contract calculates the amount of tokens to mint
 contract TokenSale is ERC20("TokenSale", "TS") {
-    uint256 public constant MAX_SUPPLY = 100_000_000 * 10 ** 10;
-    uint256 public constant PRICE_PER_UNIT = 0.001 ether / 10 ** 10;
+    uint8 public constant DECIMALS = 10;
+    uint256 public constant MAX_SUPPLY = 100_000_000 * 10 ** DECIMALS;
+    uint256 public constant PRICE_PER_UNIT = 0.001 ether / 10 ** DECIMALS;
 
     error MaxSupplyReached();
 
     function decimals() public pure override returns (uint8) {
+        return DECIMALS;
     }
 
-    receive() external payable {}
+    receive() external payable {
+        uint256 tokensToMint = msg.value / PRICE_PER_UNIT;
+        require(totalSupply() + tokensToMint <= MAX_SUPPLY, MaxSupplyReached());
+        _mint(msg.sender, tokensToMint);
+    }
 }
