8d62cc25c28fb68a06fc8dcf6d3e91e4852fefb2
Mario Karagiorgas <salem8@gmail.com>
2025-05-28 16:23:12 +0300

    feat: payment splitter

--- PaymentSplitter/src/ERC20Splitter.sol
+++ PaymentSplitter/src/ERC20Splitter.sol
@@ -7,11 +7,19 @@ import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol
 contract ERC20Splitter {
     using SafeERC20 for IERC20;
 
-    IERC20 internal immutable token;
-
     error InsufficientBalance();
     error InsufficientApproval();
     error ArrayLengthMismatch();
 
-    function split(IERC20 token, address[] calldata recipients, uint256[] calldata amounts) external {}
+    function split(IERC20 token, address[] calldata recipients, uint256[] calldata amounts) external {
+        require(recipients.length == amounts.length, ArrayLengthMismatch());
+        uint256 len = recipients.length;
+        for (uint256 i; i < len; i++) {
+            address r = recipients[i];
+            uint256 a = amounts[i];
+            require(token.allowance(msg.sender, address(this)) >= a, InsufficientApproval());
+            require(token.balanceOf(msg.sender) >= a, InsufficientBalance());
+            token.safeTransferFrom(msg.sender, r, a);
+        }
+    }
 }
--- PaymentSplitter/test/ERC20Splitter.t.sol
+++ PaymentSplitter/test/ERC20Splitter.t.sol
@@ -7,6 +7,7 @@ import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract RareSkillsToken is ERC20 {
     constructor(string memory name, string memory symbol) ERC20(name, symbol) {}
+
     function mint(address to, uint256 amount) external {
         _mint(to, amount);
     }
