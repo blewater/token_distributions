6bb9fd669010d78986e04100c4d5c63a1a0b2b36
Mario Karagiorgas <salem8@gmail.com>
2025-05-29 14:06:53 +0300

    feat: Weth

--- WETH/src/WETH.sol
+++ WETH/src/WETH.sol
@@ -4,7 +4,11 @@ pragma solidity 0.8.28;
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract WETH is ERC20("Wrapped Ether", "WETH") {
-    function deposit() external payable {}
+    function deposit() external payable {
+        _mint(msg.sender, msg.value);
+    }
 
-    function withdraw(uint256 amount) external {}
+    function withdraw(uint256 amount) external {
+        _burn(msg.sender, amount);
+    }
 }
